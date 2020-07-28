4ae8e68c31d7fc4c2f2fc1409877d24422bff3d2
Alan Novaes <alanovaes@live.com>
2020-07-28 11:36:10 -0300

    commit aula 2 finalizada

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import PageDefault from "../../../components/PageDefault";
 
 const CadastroCategoria = () => {
-  return <PageDefault>
-    <h1>Cadastro de Categoria</h1>
-    
-    <Link to="/">
-      Ir para home
-    </Link>
-  </PageDefault>;
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+
+        <button>Cadastrar</button>
+      </form>
+      <Link to="/">Ir para home</Link>{" "}
+    </PageDefault>
+  );
 };
 
 export default CadastroCategoria;
